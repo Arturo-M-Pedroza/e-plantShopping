0b4e548f0b4ceac7afdeca91167288563db4b678
Arturo Morales Pedroza <arturo.m.pedroza@gmail.com>
2025-08-11 20:47:28 -0400

    Added reducers in 'CartSlice'

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -257,6 +257,7 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (product) => {
+        console.log(product);
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
       
         setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
@@ -290,7 +291,7 @@ function ProductList({ onHomeClick }) {
                     {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                         <div key={index}> {/* Unique key for each category div */}
                             <h1>
-                            <div>{category.category}</div> {/* Display the category name */}
+                            <div style={{textAlign: 'center'}}>{category.category}</div> {/* Display the category name */}
                             </h1>
                             <div className="product-list"> {/* Container for the list of plant cards */}
                             {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
@@ -303,7 +304,7 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-title">{plant.name}</div> {/* Display plant name */}
                                 {/* Display other plant details like description and cost */}
                                 <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                                <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                                <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                                 <button
                                     className="product-button"
                                     onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
