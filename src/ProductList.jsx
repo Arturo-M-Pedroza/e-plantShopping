4e1545406bb9c12dc7651d22efb03b97f7259388
Arturo Morales Pedroza <arturo.m.pedroza@gmail.com>
2025-08-11 21:31:46 -0400

    Added CartItem

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -257,9 +260,7 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (product) => {
-        console.log(product);
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
-      
         setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
           ...prevState, // Spread the previous state to retain existing entries
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
